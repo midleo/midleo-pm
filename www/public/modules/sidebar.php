909eedb96265eb58d04a0f1d1f586c778cae7319
vaskovasilev <11207975+vaskovasilev@users.noreply.github.com>
2021-12-01 22:27:32 +0100

    update sidebar

--- www/public/modules/sidebar.php
+++ www/public/modules/sidebar.php
@@ -1,26 +1,12 @@
 <div class="sticky-top"><br>
 <h4><i class="mdi mdi-gesture-double-tap"></i>&nbsp;Navigation</h4>
 <br>
-<nav class="sidebar-nav ">
-    <ul id="sidebarnav">
-    <li class="row">
-            <a class="waves-effect waves-dark col-md-12 <?php echo $page=="dashboard"?"active":"";?>" href="/cp/?"><i class="mdi mdi-view-dashboard-outline"></i>&nbsp;Dashboard</a>
-        </li>
-        <li class="row">
-            <a class="waves-effect waves-dark col-md-12 <?php echo $thisarray["p0"]=="tickets"?"active":"";?>" href="/tickets"><i class="mdi mdi-format-list-checks"></i>&nbsp;Tickets</a>
-        </li>
-        <li class="row">
-            <a class="waves-effect waves-dark col-md-12 <?php echo $thisarray["p0"]=="projects"?"active":"";?>" href="/projects"><i class="mdi mdi-file-tree"></i>&nbsp;Projects</a>
-        </li>
-        <li class="row">
-            <a class="waves-effect waves-dark col-md-12 <?php echo $thisarray["p0"]=="kanban"?"active":"";?>" href="/kanban"><i class="mdi mdi-trello"></i>&nbsp;Kanban board</a>
-        </li>
-        <li class="row">
-            <a class="waves-effect waves-dark col-md-12 <?php echo $thisarray["p0"]=="calendar"?"active":"";?>" href="/calendar"><i class="mdi mdi-calendar-clock-outline"></i>&nbsp;Time Management</a>
-        </li>
-        <li class="row">
-            <a class="waves-effect waves-dark col-md-12 <?php echo $thisarray["p0"]=="cpinfo"?"active":"";?>" href="/cpinfo"><i class="mdi mdi-file-document-edit-outline"></i>&nbsp;Documentation</a>
-        </li>
-    </ul>
-</nav>
+<div class="list-group">
+    <a href="/cp/?" class="waves-effect waves-light list-group-item list-group-item-light list-group-item-action <?php echo $page=="dashboard"?"active":"";?>"><i class="mdi mdi-view-dashboard-outline"></i>&nbsp;Dashboard</a>
+    <a href="/tickets" class="waves-effect waves-light list-group-item list-group-item-light list-group-item-action <?php echo $thisarray["p0"]=="tickets"?"active":"";?>"><i class="mdi mdi-format-list-checks"></i>&nbsp;Tickets</a>
+    <a href="/projects" class="waves-effect waves-light list-group-item list-group-item-light list-group-item-action <?php echo $thisarray["p0"]=="projects"?"active":"";?>"><i class="mdi mdi-file-tree"></i>&nbsp;Projects</a>
+    <a href="/kanban" class="waves-effect waves-light list-group-item list-group-item-light list-group-item-action <?php echo $thisarray["p0"]=="kanban"?"active":"";?>"><i class="mdi mdi-trello"></i>&nbsp;Kanban board</a>
+    <a href="/calendar" class="waves-effect waves-light list-group-item list-group-item-light list-group-item-action <?php echo $thisarray["p0"]=="calendar"?"active":"";?>"><i class="mdi mdi-calendar-clock-outline"></i>&nbsp;Time Management</a>
+    <a href="/cpinfo" class="waves-effect waves-light list-group-item list-group-item-light list-group-item-action <?php echo $thisarray["p0"]=="cpinfo"?"active":"";?>"><i class="mdi mdi-file-document-edit-outline"></i>&nbsp;Documentation</a>
+ </div>
 </div>
